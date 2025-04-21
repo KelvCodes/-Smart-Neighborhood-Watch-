3575d8232af565ad0c1c73d446dec2ce82689c9c
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-04-21 14:51:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
- description = document.getElementById("incident-description").value;
-
-    i= document.createElement("li");
-    listItem.innerHTML = `<strong>${title}</strong> - ${description}`;
-    alertList.prepend(listItem);
 
     // Clear fields
     document.getElementById("incident-title").value = "";
